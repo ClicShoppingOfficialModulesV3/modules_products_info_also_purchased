7ebb8c5ba1c59ece805cabb8f51a3dd1119cb892
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-08-03 14:42:25 -0400

    initialize $products_tag

--- sources/template/Default/modules/modules_products_info/pi_products_info_also_purchased.php
+++ sources/template/Default/modules/modules_products_info/pi_products_info_also_purchased.php
@@ -236,6 +236,7 @@
 // display products tag
                 $tag = $CLICSHOPPING_ProductsFunctionTemplate->getProductsHeadTag($products_id);
 
+                $products_tag = '';
                 if (!is_null($tag)) {
                   foreach ($tag as $value) {
                     $products_tag .= '#<span class="productTag">' . HTML::link(CLICSHOPPING::link('index.php', 'Search&keywords='. HTML::outputProtected(utf8_decode($value) .'&search_in_description=1&categories_id=&inc_subcat=1'), 'rel="nofollow"'), $value) . '</span> ';
